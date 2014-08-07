068d149755a6128376a9d6dc942590edbb690fa5
RokKos <rocki5555@gmail.com>
2014-08-07 17:12:19 +0200

    Spam Sender
    Simple spam sender with really bare GUI

---
+++ Inkredibile Spam Sender/MainForm.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace nek_spammer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.asyncSpam = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nastavitveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izhodToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.notifyMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(499, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zapri";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 88);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(562, 71);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(209, 202);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(160, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Naključno izberi sporočilo";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Število ponovitev:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(117, 165);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(89, 22);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(341, 165);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(62, 22);
+            this.numericUpDown2.TabIndex = 8;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Čas med sporočili:";
+            // 
+            // asyncSpam
+            // 
+            this.asyncSpam.WorkerSupportsCancellation = true;
+            this.asyncSpam.DoWork += new System.ComponentModel.DoWorkEventHandler(this.asyncSpam_DoWork);
+            this.asyncSpam.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.asyncSpam_RunWorkerCompleted);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::nek_spammer.Properties.Resources.LOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Orange;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(586, 60);
+            this.panel1.TabIndex = 11;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.notifyMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Spammer";
+            this.notifyIcon.Visible = true;
+            // 
+            // notifyMenu
+            // 
+            this.notifyMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nastavitveToolStripMenuItem,
+            this.izhodToolStripMenuItem});
+            this.notifyMenu.Name = "notifyMenu";
+            this.notifyMenu.Size = new System.Drawing.Size(153, 70);
+            // 
+            // nastavitveToolStripMenuItem
+            // 
+            this.nastavitveToolStripMenuItem.Name = "nastavitveToolStripMenuItem";
+            this.nastavitveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nastavitveToolStripMenuItem.Text = "Nastavitve";
+            this.nastavitveToolStripMenuItem.Click += new System.EventHandler(this.nastavitveToolStripMenuItem_Click);
+            // 
+            // izhodToolStripMenuItem
+            // 
+            this.izhodToolStripMenuItem.Name = "izhodToolStripMenuItem";
+            this.izhodToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.izhodToolStripMenuItem.Text = "Izhod";
+            this.izhodToolStripMenuItem.Click += new System.EventHandler(this.izhodToolStripMenuItem_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 202);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(199, 17);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Pritisni enter pred in po sporočilu";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(306, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Besedilo za spam (za več sporočil - vsakega v svojo vrstico):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(409, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "sekund";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Ugasni";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(375, 202);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(118, 17);
+            this.checkBox3.TabIndex = 16;
+            this.checkBox3.Text = "Naključna številka";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 231);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(602, 378);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.TransparencyKey = System.Drawing.Color.Orange;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.notifyMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker asyncSpam;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip notifyMenu;
+        private System.Windows.Forms.ToolStripMenuItem nastavitveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izhodToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ Inkredibile Spam Sender/MainForm.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WindowsInput;
+using System.Threading;
+
+
+namespace nek_spammer
+{
+    public partial class MainForm : Form
+    {
+        private bool bc = false;
+        Random rn = new Random(DateTime.Now.Millisecond);
+        string besedilo;
+        string izpisb;
+        int steviloPonovitev;
+        int stevec;
+        int tralala;//stevc za presledke
+        int trala; //omejitev hitrosti
+        private bool dv= false;
+        private bool cd = false;
+
+        private bool isSpamming = false;
+        private bool isClosing = false;
+        
+        public MainForm()
+
+
+        {
+            InitializeComponent();
+   
+
+        }
+
+        public  void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            besedilo = textBox1.Text;
+            if (bc)
+            {
+                besedilo = besedilo +"  "+ rn.Next(100, 999)+ rn.Next(100,999);
+                
+            }
+           
+            //label1.Text = besedilo;
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            
+        }
+
+        public void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (checkBox1.Checked)
+            {
+                
+                bc = true;
+            }
+            else
+            {
+                bc = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //to lohk zbrism ^^
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+            steviloPonovitev = (int)numericUpDown1.Value;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            trala = (int)numericUpDown2.Value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (DwmApi.DwmIsCompositionEnabled())
+            {
+                DwmApi.DwmExtendFrameIntoClientArea(Handle, new DwmApi.MARGINS(0, 60, 0, 0));
+            }
+
+            Keys hotKey = Keys.S | Keys.Control | Keys.Shift;
+            WindowsShell.RegisterHotKey(this, hotKey);
+            Thread.Sleep(1000);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isClosing)
+            {
+                WindowsShell.UnregisterHotKey(this);
+            }
+
+            else
+            {
+                Hide();
+                e.Cancel = true;
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WindowsShell.WM_HOTKEY)
+            {
+                if (isSpamming)
+                {
+                    isSpamming = false;
+                }
+
+                else
+                {
+                    isSpamming = true;
+                    asyncSpam.RunWorkerAsync();
+                }
+            }
+        }
+
+        private void BeginSpam()
+        {
+            
+        }
+
+        private void asyncSpam_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string[] sporocila = textBox1.Text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            // rnd.Next(sporocila.Length);
+            
+            for (stevec = 0; stevec < steviloPonovitev; stevec++)
+            {
+                if (!isSpamming)
+                    break;
+
+                if (bc)
+                {
+                    izpisb = sporocila[rn.Next(sporocila.Length)];
+                }
+
+                else
+                {
+                    izpisb = sporocila[0];
+                }
+
+                if (cd)
+                {
+                    izpisb = izpisb + "   " + rn.Next(100, 999) + rn.Next(100, 999);
+                }
+
+
+                if (dv)
+                {
+                    InputSimulator.SimulateKeyPress(VirtualKeyCode.RETURN);
+                }
+
+                InputSimulator.SimulateTextEntry(izpisb);
+                Thread.Sleep(50);
+                InputSimulator.SimulateKeyPress(VirtualKeyCode.RETURN);
+
+                Thread.Sleep(trala * 1000);
+                    
+            }
+
+            isSpamming = false;
+        }
+
+        private void asyncSpam_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void izhodToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            isClosing = true;
+            Close();
+        }
+
+        private void nastavitveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Show();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                dv = true;
+            }
+            else
+            {
+                dv = false;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            isClosing = true;
+            Close();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+            {
+                cd = true;
+            }
+            else
+            {
+                cd = false;
+            }
+        }
+    }
+}
